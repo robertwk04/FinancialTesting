277314337258dd1c0ea2e66a141ddde857a197b1
Ionesie Wyky-Robert <ionesierobert2004s3@gmail.com>
2025-11-27 21:09:19 +0200

    incercare noua

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vite'
+// vite.config.js
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: './lib/main.js',
-      name: 'Counter',
-      fileName: 'counter',
-    },
-  },
-})
+  base: '/FinancialTesting/', 
+});
